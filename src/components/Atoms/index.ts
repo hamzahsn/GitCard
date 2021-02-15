29c075ec64dc96cd043cc34ed73ddb6a6ea8b831
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-15 05:28:32 +0100

    Exporting components for usage

---
+++ src/components/Atoms/index.ts
@@ -0,0 +1,6 @@
+export { LabelInput } from './LabelInput/LabelInput'
+export { Button } from './Button/Button'
+export { Spinner } from './Spinner/Spinner'
+export { Typography } from './Typography/Typography'
+export { ColorPicker } from './ColorPicker/ColorPicker'
+export { IconsPicker } from './IconPicker/IconPicker'
