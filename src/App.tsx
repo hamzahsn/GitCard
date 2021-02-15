005478d5428b147e35fcc5e783421ad88765c1a6
hamzahsn <amdouni.hamza@hotmail.fr>
2021-02-15 05:42:27 +0100

    Lazy load the pages in App comonent with react suspense and enabling routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+const CreateProfile = React.lazy(() => import('./components/Pages/CreateProfile/CreateProfile'))
+const ViewProfile = React.lazy(() => import('./components/Pages/ProfileViewer/ProfileViewer'))
 
 import './App.scss'
+import { Spinner } from '@atoms/index'
 
 export function App() {
-  return <div>Aloha!</div>
+  return (
+    <Suspense fallback={<Spinner />}>
+      <Router>
+        <Switch>
+          <Route path="/" component={CreateProfile} exact />
+          <Route path="/r/:magicLink" component={ViewProfile} exact />
+        </Switch>
+      </Router>
+    </Suspense>
+  )
 }
